bc29589fbd7d45059cfce13ec968ae618cfcdef4
Asad Ullah <developerfrontend786@gmail.com>
2024-12-08 23:30:58 +0500

    crdu Add

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-function TaskList({todoList, deleteTask, completedTask }) {
+function TaskList({todoList, deleteTask, editTask }) {
     return(
         todoList.length>=1?
         <div>
             <h3>Task List</h3>
         {todoList.map((task,index)=>(
-            <div key={index} className={todoList.completed?'complete':''}>
+            <div key={index}>
                 <p className="task">{task.task}</p>
               <div> 
                  <button onClick={() => deleteTask(task.id)} className="del-button">Delete &times;</button>
-                 <button onClick={completedTask(index)} >Done</button>
+                 <button onClick={()=>editTask(index)} >Done</button>
                  </div>
                 </div>
         ))}</div>:null
--- src/TodoList.js
+++ src/TodoList.js
@@ -6,40 +6,26 @@ function TodoList(){
     const [task, setTask]= useState('');
     const [todoList, setTodoList]=useState([]);
     function handleChange(e){
-        setTask(e.target.value)
+        setTask(e.target.value);
+        setTask('')
     }
     function addList(){
         const newTask={
             id : todoList.length===0?1:todoList[todoList.length-1].id+1,
             task,
-            completed:false
         }
         setTodoList([...todoList, newTask]);
     }
-    function deleteTask(id){
-        setTodoList(todoList.filter(task => task.id !== id));
-    }
-   
-    
-    function completedTask(id){
-       setTodoList(
-        todoList.map((task)=>{
-            if(task.id === id){
-                return {...task, completed: !task.completed}
-            } else{
-                return task
-            }
-    })
-       )
-       console.log(todoList.completed);
-       
+    // Edit Task
+    function editTask(id, newTask){
+        setTodoList(todoList.map(task => task.id === id ? { ...task, task: newTask } : task));
     }
 
                 
     return(
         <div>
             <InputList handleChange={handleChange} addList={addList} task={task} />
-            <TaskList todoList={todoList} deleteTask={deleteTask} completedTask={completedTask} />
+            <TaskList todoList={todoList} editTask={editTask} />
         </div>
 
     )
